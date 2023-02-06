f15eb7fdfc93c9ade2250a7039f971063139334e
AndrewSushi <AndrewSushi8@gmail.com>
2023-02-05 23:50:33 -0800

    Finished isPathClear()

--- move-logic.js
+++ move-logic.js
@@ -2,6 +2,8 @@ let enPassant = false
 let enPassantSquare = null
 let whiteTurn = true
 
+let castling = 15 // Binary representation of 0x1111 rooks 0xwL wR bL bR
+
 // Piece Moving
 function movePiece(fromX, fromY, toX, toY){
     if(isValidMove(fromX, fromY, toX, toY)){
@@ -14,12 +16,39 @@ function movePiece(fromX, fromY, toX, toY){
 }
 
 function isPathClear(fromX, fromY, toX, toY){
-    
+    x = toX - fromX
+    y = toY - fromY
+    if(x == 0){
+        let increment = y > 0 ? 1 : -1;
+        for(let i = fromY + increment; i != toY; i += increment){
+            if(board[fromX][i] != ''){
+                return false;
+            }
+        }
+    }else if(y == 0){
+        let increment = x > 0 ? 1 : -1;
+        for(let i = fromX + increment; i != toX; i += increment){
+            if(board[i][fromY] != ''){
+                return false
+            }
+        }
+    }else if(Math.abs(x) == Math.abs(y)){
+        let incrementY = y > 0 ? 1 : -1;
+        let incrementX = x > 0 ? 1 : -1;
+        for(let i = fromX + incrementX, j = fromY + incrementY; i != toX; i += incrementX, j += incrementY){
+            if(board[i][j] != ''){
+                return false
+            }
+        }
+    }else{
+        return false
+    }
+    return true
 }
 
 function rook(fromX, fromY, toX, toY, piece){
     move1 = (fromX == toX || fromY == toY)
-    return move1
+    return move1 & isPathClear(fromX, fromY, toX, toY)
 }
 
 function knight(fromX, fromY, toX, toY){
@@ -30,20 +59,35 @@ function knight(fromX, fromY, toX, toY){
 
 function bishop(fromX, fromY, toX, toY){
     move1 = (Math.abs(fromX - toX) == Math.abs(fromY - toY))
-    return move1
+    return move1 & isPathClear(fromX, fromY, toX, toY)
 }
 
 function queen(fromX, fromY, toX, toY){
     move1 = (fromX == toX || fromY == toY)
     move2 = (Math.abs(fromX - toX) == Math.abs(fromY - toY))
-    return move1 || move2
+    return (move1 || move2) & isPathClear(fromX, fromY, toX, toY)
 }
 
-function king(fromX, fromY, toX, toY){
+function king(fromX, fromY, toX, toY, piece){
+    if(piece[0] == 'w' && (castling >> 2 > 0)){
+        if(Math.abs(fromY - toY) == 2){
+            console.log("ASFHJKDHJKFASDHJIKFBk")
+        }
+    }
     move1 = (fromX == toX || fromY == toY)
     move2 = (Math.abs(fromX - toX) == Math.abs(fromY - toY))
     oneSquare = ((Math.abs(fromX - toX) == 1) || (Math.abs(fromX - toX) == 0)) && ((Math.abs(fromY - toY) == 1) || (Math.abs(fromY - toY) == 0))
-    return (move1 || move2) && oneSquare
+    if((move1 || move2) && oneSquare){
+        if(piece[0] == 'w'){
+            castling = castling & 0b0011
+        }else{
+            castling = castling & 0b1100
+        }
+        return true
+    }
+
+
+    return false
 }
 
 function pawn(fromX, fromY, toX, toY, piece){
